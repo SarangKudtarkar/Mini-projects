315868a22240516eabe017e126d991bab46b39f3
poet4life <40836487+poet4life@users.noreply.github.com>
2019-09-04 20:36:01 +0530

    Update fib.cpp

--- fib.cpp
+++ fib.cpp
@@ -1,7 +1,10 @@
 #include <bits/stdc++.h>
+#include<chrono>
 using namespace std;
+using namespace std::chrono;
 void fib(unsigned long int a)
 {
+    auto start = high_resolution_clock::now();
     unsigned long int t,g0,g1;
     if(a<=1){cout<<a<<"\n";return;}
     g0=1;
@@ -14,10 +17,13 @@ void fib(unsigned long int a)
     }
 
        cout<<g1<<"\n";
-
+auto stop = high_resolution_clock::now();
+auto duration = duration_cast<microseconds>(stop - start);
+cout << "Time taken by modified method "<<duration.count() << endl;
 }
 unsigned long int fibo(unsigned long int a)
 {
+
     if(a<=1)return a;
     return (fibo(a-1)+fibo(a-2));
 }
@@ -26,8 +32,11 @@ unsigned long int n;
 
 cin>>n;
 fib(n);
-//cout<<fibo(n);
-
+auto start = high_resolution_clock::now();
+cout<<fibo(n)<<"\n";
+auto stop = high_resolution_clock::now();
+auto duration = duration_cast<microseconds>(stop - start);
+cout << "Time taken by simple method "<<duration.count() << endl;
 	return 0;
 }
 
